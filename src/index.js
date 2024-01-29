050c25d74cb13dec074da7ab02621c71cb8e1a35
= <appleiban@icloud.com>
2024-01-29 15:00:16 +0300

    5 commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
+$(document).ready(function () {
+  $("p").click(function () {
+    $(this).hide();
+  });
+  $("button").click(function () {
+    $("p").hide(1000);
+  });
 });
